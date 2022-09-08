22734582fc458a32fa95b74626aed1790d236f60
pariss1358@gmail.com <pariss1358@gmail.com>
2022-09-07 18:57:27 -0700

    temp

--- loan_pred.py
+++ loan_pred.py
@@ -51,5 +51,5 @@ from sklearn.model_selection import train_test_split
 x_train, x_test, y_train, y_test = train_test_split(x_resample, y_resample, test_size = 0.2, random_state = 0)
 model =  GradientBoostingClassifier()
 model.fit(x_train, y_train)
-file = open('loan_status.pkl', 'wb')
+file = open('Forest.pkl', 'wb')
 pickle.dump(model, file)
